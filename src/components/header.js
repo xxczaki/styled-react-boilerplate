182e0babde11d259b533562f244889fdcaed4ad2
Antoni Kepinski <keppi@o2.pl>
2019-02-15 23:06:52 +0100

    Hot reloading for Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
+import {hot} from 'react-hot-loader/root';
 
+// Header
 const Header = styled.h1`
     font-size: 25px;
 `;
 
-export default Header;
+export default hot(Header);
