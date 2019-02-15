89d88540c5fb7ba36dda14ae4f459b0fafff54b7
Antoni Kepinski <keppi@o2.pl>
2019-02-15 23:05:05 +0100

    Optimize Webpack Configuration :zap:

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,37 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractCssChunks = require('extract-css-chunks-webpack-plugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
 	output: {
-		filename: 'main.js',
+		filename: '[name].[hash].js',
 		path: path.resolve(__dirname, 'dist')
+	},
+	optimization: {
+		runtimeChunk: 'single',
+		splitChunks: {
+			chunks: 'all',
+			maxInitialRequests: Infinity,
+			minSize: 0,
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name(module) {
+						const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+						return `npm.${packageName.replace('@', '')}`;
+					}
+				}
+			}
+		}
 	},
 	devServer: {
 		contentBase: path.join(__dirname, 'dist'),
 		compress: true,
+		quiet: true,
 		hot: true
 	},
 	module: {
@@ -21,12 +42,25 @@ module.exports = {
 				loader: 'babel-loader'
 			},
 			{
+				test: /\.jsx?$/,
+				include: /node_modules/,
+				use: ['react-hot-loader/webpack']
+			},
+			{
+				test: /\.css$/,
+				use: [
+					ExtractCssChunks.loader,
+					'css-loader'
+				]
+			},
+			{
 				test: /\.(pdf|jpg|png|gif|svg|ico)$/,
 				use: [
 					'file-loader',
 					{
 						loader: 'img-loader',
 						options: {
+							outputPath: 'images/',
 							plugins: [
 								require('imagemin-mozjpeg')({
 									progressive: true
@@ -39,6 +73,16 @@ module.exports = {
 						}
 					}
 				]
+			},
+			{
+				test: /\.(woff2|woff)$/,
+				use: [{
+					loader: 'file-loader',
+					options: {
+						name: '[name].[ext]',
+						outputPath: 'fonts/'
+					}
+				}]
 			}
 		]
 	},
@@ -55,9 +99,19 @@ module.exports = {
 				useShortDoctype: true
 			}
 		}),
+		new ExtractCssChunks(
+			{
+				filename: '[name].css',
+				chunkFilename: '[id].css',
+				hot: true
+			}
+		),
+		new webpack.optimize.ModuleConcatenationPlugin(),
+		new webpack.HashedModuleIdsPlugin(),
 		new ScriptExtHtmlWebpackPlugin({
 			prefetch: /\.js$/,
 			defaultAttribute: 'async'
-		})
+		}),
+		new FriendlyErrorsWebpackPlugin()
 	]
 };
