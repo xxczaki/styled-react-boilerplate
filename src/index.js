ede26612139ed0a247002a9d9ed3bbd71598ea53
Antoni Kepinski <keppi@o2.pl>
2019-02-15 23:05:52 +0100

    Load fonts & modern-normalize locally & change the example page

--- src/index.js
+++ src/index.js
@@ -2,30 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createGlobalStyle} from 'styled-components';
 
+// Import modern-normalize & fonts
+import 'typeface-open-sans';
+import 'modern-normalize/modern-normalize.css';
+
 // Import Components
 import Container from './components/container';
 import Header from './components/header';
+import Counter from './components/counter';
 
 // Global Style
 const GlobalStyle = createGlobalStyle`
   body {
+    font-family: Open Sans, monospace !important;
     background: #212121;
     color: #fff;
     padding: 1em;
     line-height: 1.8em;
-		font-size: 15;
     -webkit-font-smoothing: antialiased;
     text-rendering: optimizeSpeed;
     word-wrap: break-word
   }
 `;
 
-// Render page
-ReactDOM.render(
+// Main page
+const App = () => (
 	<Container>
 		<Header>Hello World ⚡</Header>
-		<p>Example site using Styled React Boilerplate</p>
+		<p>Example site using Styled React Boilerplate!</p>
+		<Counter/>
 		<GlobalStyle/>
-	</Container>,
-	document.getElementById('root')
+	</Container>
+);
+
+// Render page
+ReactDOM.render(
+	<App/>,
+	document.querySelector('#root')
 );
