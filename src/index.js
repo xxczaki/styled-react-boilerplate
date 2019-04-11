74028f4c12c0a31ad8411c79221b526b3bc5689d
Antoni Kepinski <xxczaki@pm.me>
2019-04-11 19:11:39 +0200

    Use React DOM in separate file

--- src/index.js
+++ src/index.js
@@ -1,46 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {createGlobalStyle} from 'styled-components';
-import * as OfflinePluginRuntime from 'offline-plugin/runtime';
+import {render} from 'react-dom';
+import App from './app';
 
-// Import modern-normalize & fonts
-import 'typeface-open-sans';
-import 'modern-normalize/modern-normalize.css';
+const root = document.createElement('div');
+document.body.append(root);
 
-// Import Components
-import Container from './components/container';
-import Header from './components/header';
-import Counter from './components/counter';
-
-// Install Service Worker
-OfflinePluginRuntime.install();
-
-// Global Style
-const GlobalStyle = createGlobalStyle`
-  body {
-    font-family: Open Sans, monospace !important;
-    background: #212121;
-    color: #fff;
-    padding: 1em;
-    line-height: 1.8em;
-    -webkit-font-smoothing: antialiased;
-    text-rendering: optimizeSpeed;
-    word-wrap: break-word
-  }
-`;
-
-// Main page
-const App = () => (
-	<Container>
-		<Header>Hello World ⚡</Header>
-		<p>Example site using Styled React Boilerplate!</p>
-		<Counter/>
-		<GlobalStyle/>
-	</Container>
-);
-
-// Render page
-ReactDOM.render(
-	<App/>,
-	document.querySelector('#root')
-);
+render(<App/>, root);
