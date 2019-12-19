cf5d4b302868f88c020413ec74c4fea18fd87e92
Antoni Kepinski <xxczaki@pm.me>
2019-12-19 19:15:25 +0100

    register service worker

--- src/index.js
+++ src/index.js
@@ -5,4 +5,15 @@ import App from './app';
 const root = document.createElement('div');
 document.body.append(root);
 
+// Register service worker
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker.register('/sw.js').then(registration => {
+			console.log('SW registered:', registration);
+		}).catch(error => {
+			console.log('SW registration failed:', error);
+		});
+	});
+}
+
 render(<App/>, root);
